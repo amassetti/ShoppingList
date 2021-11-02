9be681118a5f97f595bcd212b7409b6bf7387840
Ariel <amassetti@gmail.com>
2021-11-02 09:52:15 -0300

    Adding mongo config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-  mongoURI:  process.env.MONGO_URI,
+  mongoURI:  process.env.MONGO_URI || 'mongodb+srv://ariel:Asd$123!@mflix.fy047.mongodb.net/test',
   port: process.env.PORT || 5000
 }
